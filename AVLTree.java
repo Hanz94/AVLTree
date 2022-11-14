bc46271571ca15fdad03d09a7b27b0a82991e453
hanz94 <hansikam94@gmail.com>
2022-11-13 22:47:50 -0500

    implement delete by key in binary tree

--- AVLTree.java
+++ AVLTree.java
@@ -67,9 +67,48 @@ abstract class Tree<T extends Node<T>> {
         return root;
     }
 
-    public boolean delete(){
+    public Stack<T> delete(int key) {
+        Stack<T> stk = new Stack<>();
+        delete(key, this.root, stk);
+        return stk;
+    }
 
-        return false;
+    private T delete(int key, T root, Stack<T> stk) {
+        if (root == null) {
+            return root;
+        }
+        else if (key < root.getKey()){
+            stk.push(root.getLeft());
+            root.setLeft(delete(key, root.getLeft(), stk));
+        }
+        else if (key > root.getKey()){
+            stk.push(root.getRight());
+            root.setRight(delete(key, root.getRight(), stk));
+        }
+        else {
+            if (root.getLeft() == null) {
+                return root.getRight();
+            }
+            else if (root.getRight() == null) {
+                return root.getLeft();
+            }
+            else {
+                root.setKey(getMax(root.getLeft()));
+                root.setLeft(delete(root.getKey(), root.getLeft(), stk));
+            }
+        }
+        return root;
+    }
+
+    int getMax(T root)
+    {
+        int max = root.getKey();
+        while (root.getRight() != null)
+        {
+            max = root.getRight().getKey();
+            root = root.getRight();
+        }
+        return max;
     }
 
     public int search(int key){
