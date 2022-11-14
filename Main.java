679c7de9bda9076ac4c60c044a24563e3a3e89be
hanz94 <hansikam94@gmail.com>
2022-11-13 22:48:17 -0500

    testing for insert operation

--- Main.java
+++ Main.java
@@ -2,21 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AVLNode rootNode = new AVLNode(1);
         AVLTree testAvlTree = AVLTree.initialize();
+        testAvlTree.insert(4);
+        testAvlTree.insert(3);
+        testAvlTree.insert(5);
+        testAvlTree.insert(1);
+        testAvlTree.insert(10);
 
-        System.out.println("Height at root: " + rootNode.getHeight());
-        System.out.println("Balance at root: " + rootNode.getBalance());
-
-        rootNode.setLeft(new AVLNode(2));
-
-        System.out.println("Height at root: " + rootNode.getHeight());
-        System.out.println("Balance at root: " + rootNode.getBalance());
-
-        rootNode.getLeft().setLeft(new AVLNode(3));
-
-        System.out.println("Height at root: " + rootNode.getHeight());
-        System.out.println("Balance at root: " + rootNode.getBalance());
+        System.out.println("Height at root: " + testAvlTree);
+//        System.out.println("Balance at root: " + rootNode.getBalance());
+//
+//        rootNode.setLeft(new AVLNode(2));
+//
+//        System.out.println("Height at root: " + rootNode.getHeight());
+//        System.out.println("Balance at root: " + rootNode.getBalance());
+//
+//        rootNode.getLeft().setLeft(new AVLNode(3));
+//
+//        System.out.println("Height at root: " + rootNode.getHeight());
+//        System.out.println("Balance at root: " + rootNode.getBalance());
 
     }
 
