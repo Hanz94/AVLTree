0e7a2d8f0a01b409ecf6a043c4613b058d37aba2
hanz94 <hansikam94@gmail.com>
2022-11-14 18:04:35 -0500

    test cases for AVL initialize, insert, delete and search.

--- Main.java
+++ Main.java
@@ -8,20 +8,28 @@ public class Main {
         testAvlTree.insert(5);
         testAvlTree.insert(1);
         testAvlTree.insert(10);
+        System.out.println(testAvlTree);
 
+        testAvlTree.insert(20);
+        System.out.println(testAvlTree);
+
+        testAvlTree.insert(30);
         System.out.println(testAvlTree);
 
         testAvlTree.delete(3);
-
         System.out.println(testAvlTree);
 
         testAvlTree.delete(4);
-
         System.out.println(testAvlTree);
 
         testAvlTree.insert(4);
-
         System.out.println(testAvlTree);
+
+        System.out.println(testAvlTree.search(10));
+        System.out.println(testAvlTree.search(11));
+
+        System.out.println(testAvlTree.searchRange(2, 6));
+        System.out.println(testAvlTree.searchRange(10, 20));
 
     }
 
