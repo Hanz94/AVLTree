b644bd1218e32d5d3cac4f9112b84068e12a71d5
hanz94 <hansikam94@gmail.com>
2022-11-14 20:58:58 -0500

    Refactor

---
+++ AVLBinaryTree.java
@@ -0,0 +1,308 @@
+import java.util.*;
+import java.util.function.Supplier;
+
+public class AVLBinaryTree extends BinaryTree<AVLNode> {
+
+    private AVLBinaryTree() {
+        super(AVLNode::new);
+    }
+
+    public static AVLBinaryTree initialize(){
+        return new AVLBinaryTree();
+    }
+
+    @Override
+    public Stack<AVLNode> insert(int key) {
+        Stack<AVLNode> nodeTrace = super.insert(key);
+
+        if (nodeTrace.size() > 2) {
+            AVLNode grandChild = nodeTrace.pop();
+            AVLNode child = nodeTrace.pop();
+            AVLNode aNode = nodeTrace.pop();
+            AVLNode root = balanceTreeOnInsert(grandChild, child, aNode);
+            while ( root == null && nodeTrace.size() > 0) {
+                grandChild = child;
+                child = aNode;
+                aNode = nodeTrace.pop();
+                root = balanceTreeOnInsert(grandChild, child, aNode);
+            }
+            if(root != null){
+                try {
+                    AVLNode parent = nodeTrace.pop();
+                    if (parent.getKey() > root.getKey()) {
+                        parent.setLeft(root);
+                    } else {
+                        parent.setRight(root);
+                    }
+                } catch (EmptyStackException e){
+                    setRoot(root);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Stack<AVLNode> delete(int key) {
+        Stack<AVLNode> nodeTrace =  super.delete(key);
+        while ( nodeTrace.size() > 0) {
+            AVLNode root = balanceTreeOnDelete(nodeTrace.pop());
+            if(root != null){
+                try {
+                    AVLNode parent = nodeTrace.peek();
+                    if (parent.getKey() > root.getKey()) {
+                        parent.setLeft(root);
+                    } else {
+                        parent.setRight(root);
+                    }
+                } catch (EmptyStackException e){
+                    setRoot(root);
+                }
+            }
+        }
+        return null;
+    }
+
+    private AVLNode balanceTreeOnInsert(AVLNode grandChild, AVLNode child, AVLNode aNode){
+        int balance = aNode.getBalance();
+
+        // Case 1 : LL
+        if (balance > 1 && grandChild.getKey() < child.getKey()){
+            return rightRotate(aNode);
+        }
+        // Case 2 : RR
+        if (balance < -1 && grandChild.getKey() > child.getKey()){
+            return leftRotate(aNode);
+        }
+        // Case 3 : LR
+        if (balance > 1 && grandChild.getKey() > child.getKey()) {
+            aNode.setLeft(leftRotate(child));
+            return rightRotate(aNode);
+        }
+
+        // Case 4 : RL
+        if (balance < -1 && grandChild.getKey() < child.getKey()) {
+            aNode.setRight(rightRotate(child));
+            return leftRotate(aNode);
+        }
+        return null;
+    }
+
+    private AVLNode balanceTreeOnDelete(AVLNode aNode){
+        int balance = aNode.getBalance();
+
+        // Case 1 : R0 and R1
+        if ( balance > 1 && aNode.getLeft().getBalance() >= 0){
+            return rightRotate(aNode);
+        }
+        // Case 2 : L-1 and L0
+        if (balance < -1 && aNode.getRight().getBalance() <= 0){
+            return leftRotate(aNode);
+        }
+        // Case 3 : R-1
+        if (balance > 1 && aNode.getLeft().getBalance() < 0) {
+            aNode.setLeft(leftRotate(aNode.getLeft()));
+            return rightRotate(aNode);
+        }
+        // Case 4 : L
+        if (balance < -1 && aNode.getRight().getBalance() > 0) {
+            aNode.setRight(rightRotate(aNode.getRight()));
+            return leftRotate(aNode);
+        }
+        return null;
+    }
+
+
+    private AVLNode rightRotate(AVLNode root) {
+        AVLNode child = root.getLeft();
+        AVLNode childRSubTree = child.getRight();
+        child.setRight(root);
+        root.setLeft(childRSubTree);
+        return child;
+    }
+
+    private AVLNode leftRotate(AVLNode root) {
+        AVLNode child = root.getRight();
+        AVLNode childLSubTree = child.getLeft();
+        child.setLeft(root);
+        root.setRight(childLSubTree);
+        return child;
+    }
+
+}
+
+
+abstract class BinaryTree<T extends Node<T>> {
+
+    private T root;
+    private final Supplier<? extends T> ctor;
+
+    public Stack<T> insert(int key){
+        Stack<T> stk = new Stack<>();
+        this.root = insert(key, this.root, stk);
+        return stk;
+    }
+
+    public Stack<T> delete(int key) {
+        Stack<T> stk = new Stack<>();
+        delete(key, this.root, stk);
+        return stk;
+    }
+
+    public Integer search(int key) {
+        T searchedNode = search(key, this.root);
+        return searchedNode != null ? searchedNode.getKey() : null;
+    }
+
+    public List<Integer> searchRange(int smallKey, int bigKey){
+        List<Integer> keys = new ArrayList<>();
+        searchRange(smallKey, bigKey, this.root, keys);
+        return keys;
+    }
+
+    @Override
+    public String toString() {
+        return traversePreOrder(this.root);
+    }
+
+
+    private T insert(int key, T root, Stack<T> stk){
+        if (root != null) {
+            stk.push(root);
+        }
+        if(root == null){
+            root = ctor.get();
+            root.setKey(key);
+            stk.push(root);
+            return root;
+        }
+        else if (key < root.getKey()){
+            root.setLeft(insert(key, root.getLeft(), stk));
+        }
+        else {
+            root.setRight(insert(key, root.getRight(), stk));
+        }
+        return root;
+    }
+
+    private T delete(int key, T root, Stack<T> stk) {
+
+        if (root == null) {
+            return null;
+        }
+        else {
+            if (key < root.getKey()){
+                stk.push(root);
+                root.setLeft(delete(key, root.getLeft(), stk));
+            }
+            else if (key > root.getKey()){
+                stk.push(root);
+                root.setRight(delete(key, root.getRight(), stk));
+            }
+            else {
+                if (root.getLeft() == null) {
+                    return root.getRight();
+                }
+                else if (root.getRight() == null) {
+                    return root.getLeft();
+                }
+                else {
+                    root.setKey(getMax(root.getLeft()));
+                    root.setLeft(delete(root.getKey(), root.getLeft(), stk));
+                }
+            }
+        }
+        return root;
+    }
+
+    private T search(int key, T root){
+        if(root == null || key == root.getKey()){
+            return root;
+        }
+        else if (key < root.getKey()){
+            return search(key, root.getLeft());
+        }
+        else {
+            return search(key, root.getRight());
+        }
+    }
+
+    private void searchRange(int smallKey, int bigKey, T root, List<Integer> keys) {
+        if (root == null) {
+            return;
+        }
+        if (smallKey < root.getKey()) {
+            searchRange( smallKey, bigKey, root.getLeft(), keys);
+        }
+        if (smallKey <= root.getKey() && bigKey >= root.getKey()) {
+            keys.add(root.getKey());
+        }
+        searchRange(smallKey, bigKey, root.getRight(), keys);
+    }
+
+    private int getMax(T root)
+    {
+        int max = root.getKey();
+        while (root.getRight() != null)
+        {
+            max = root.getRight().getKey();
+            root = root.getRight();
+        }
+        return max;
+    }
+
+    public String traversePreOrder(T root) {
+
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.getKey());
+
+        String pointerRight = "└──";
+        String pointerLeft = (root.getRight() != null) ? "├──" : "└──";
+
+        traverseNodes(sb, "", pointerLeft, root.getLeft(), root.getRight() != null);
+        traverseNodes(sb, "", pointerRight, root.getRight(), false);
+
+        return sb.toString();
+    }
+
+    private void traverseNodes(StringBuilder sb, String padding, String pointer, T node, boolean hasRightSibling) {
+        if (node != null) {
+            sb.append("\n");
+            sb.append(padding);
+            sb.append(pointer);
+            sb.append(node.getKey());
+
+            StringBuilder paddingBuilder = new StringBuilder(padding);
+            if (hasRightSibling) {
+                paddingBuilder.append("│  ");
+            } else {
+                paddingBuilder.append("   ");
+            }
+
+            String paddingForBoth = paddingBuilder.toString();
+            String pointerRight = "└──";
+            String pointerLeft = (node.getRight() != null) ? "├──" : "└──";
+
+            traverseNodes(sb, paddingForBoth, pointerLeft, node.getLeft(), node.getRight() != null);
+            traverseNodes(sb, paddingForBoth, pointerRight, node.getRight(), false);
+        }
+    }
+
+    protected BinaryTree(Supplier<? extends T> ctor) {
+        this.ctor = Objects.requireNonNull(ctor);
+        this.root = null;
+    }
+
+    protected T getRoot() {
+        return root;
+    }
+
+    protected void setRoot(T root) {
+        this.root = root;
+    }
+
+}
--- AVLTree.java
+++ AVLTree.java
@@ -1,308 +1,100 @@
-import java.util.*;
-import java.util.function.Supplier;
+import java.io.*;
+import java.util.List;
 
-public class AVLTree extends BinaryTree<AVLNode> {
+/**
+ * dtest
+ */
+public class AVLTree {
 
-    private AVLTree() {
-        super(AVLNode::new);
-    }
-
-    public static AVLTree initialize(){
-        return new AVLTree();
-    }
-
-    @Override
-    public Stack<AVLNode> insert(int key) {
-        Stack<AVLNode> nodeTrace = super.insert(key);
-
-        if (nodeTrace.size() > 2) {
-            AVLNode grandChild = nodeTrace.pop();
-            AVLNode child = nodeTrace.pop();
-            AVLNode aNode = nodeTrace.pop();
-            AVLNode root = balanceTreeOnInsert(grandChild, child, aNode);
-            while ( root == null && nodeTrace.size() > 0) {
-                grandChild = child;
-                child = aNode;
-                aNode = nodeTrace.pop();
-                root = balanceTreeOnInsert(grandChild, child, aNode);
+    public static void main(String[] args) {
+        String filename = "input_file.txt";
+        if (args.length > 0) {
+            filename = args[0];
+        }
+        AVLBinaryTree avlBinaryTree = null;
+        BufferedReader reader;
+        BufferedWriter writer;
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            writer = new BufferedWriter(new FileWriter("output_file.txt", false));
+            String line = reader.readLine();
+            while (line != null) {
+                avlBinaryTree = decodeAndRunCommands(line, avlBinaryTree, writer);
+                line = reader.readLine();
             }
-            if(root != null){
-                try {
-                    AVLNode parent = nodeTrace.pop();
-                    if (parent.getKey() > root.getKey()) {
-                        parent.setLeft(root);
-                    } else {
-                        parent.setRight(root);
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static AVLBinaryTree decodeAndRunCommands(String command, AVLBinaryTree avlBinaryTree, BufferedWriter writer) throws IOException {
+        String[] cmd = command.split("[\\(\\)]");
+        switch(cmd[0]) {
+            case "Initialize":
+                avlBinaryTree = AVLBinaryTree.initialize();
+            break;
+            case "Insert":
+                avlBinaryTree.insert(Integer.parseInt(cmd[1]));
+                break;
+            case "Delete":
+                avlBinaryTree.delete(Integer.parseInt(cmd[1]));
+                break;
+            case "Search":
+                String[] range = cmd[1].split(",");
+                if(range.length > 1){
+                    List<Integer> results = avlBinaryTree.searchRange(Integer.parseInt(range[0]), Integer.parseInt(range[1]));
+                    if (!results.isEmpty()){
+                        writer.write(results.toString().replaceAll("[\\[\\] ]", ""));
+                    }else {
+                        writer.write("NULL");
                     }
-                } catch (EmptyStackException e){
-                    setRoot(root);
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public Stack<AVLNode> delete(int key) {
-        Stack<AVLNode> nodeTrace =  super.delete(key);
-        while ( nodeTrace.size() > 0) {
-            AVLNode root = balanceTreeOnDelete(nodeTrace.pop());
-            if(root != null){
-                try {
-                    AVLNode parent = nodeTrace.peek();
-                    if (parent.getKey() > root.getKey()) {
-                        parent.setLeft(root);
-                    } else {
-                        parent.setRight(root);
+                }else {
+                    Integer result = avlBinaryTree.search(Integer.parseInt(cmd[1]));
+                    if(result != null){
+                        writer.write(result.toString());
+                    }else {
+                        writer.write("NULL");
                     }
-                } catch (EmptyStackException e){
-                    setRoot(root);
                 }
-            }
+                writer.newLine();
+                break;
+            default:
+                throw new RuntimeException(cmd[0] + " is not supported, only Initialize, Insert, Delete and Search are supported.");
         }
-        return null;
+        return avlBinaryTree;
     }
 
-    private AVLNode balanceTreeOnInsert(AVLNode grandChild, AVLNode child, AVLNode aNode){
-        int balance = aNode.getBalance();
+    public static void testAVLTree(){
+        AVLBinaryTree testAvlBinaryTree = AVLBinaryTree.initialize();
+        testAvlBinaryTree.insert(4);
+        testAvlBinaryTree.insert(3);
+        testAvlBinaryTree.insert(5);
+        testAvlBinaryTree.insert(1);
+        testAvlBinaryTree.insert(10);
+        System.out.println(testAvlBinaryTree);
 
-        // Case 1 : LL
-        if (balance > 1 && grandChild.getKey() < child.getKey()){
-            return rightRotate(aNode);
-        }
-        // Case 2 : RR
-        if (balance < -1 && grandChild.getKey() > child.getKey()){
-            return leftRotate(aNode);
-        }
-        // Case 3 : LR
-        if (balance > 1 && grandChild.getKey() > child.getKey()) {
-            aNode.setLeft(leftRotate(child));
-            return rightRotate(aNode);
-        }
+        testAvlBinaryTree.insert(20);
+        System.out.println(testAvlBinaryTree);
 
-        // Case 4 : RL
-        if (balance < -1 && grandChild.getKey() < child.getKey()) {
-            aNode.setRight(rightRotate(child));
-            return leftRotate(aNode);
-        }
-        return null;
-    }
+        testAvlBinaryTree.insert(30);
+        System.out.println(testAvlBinaryTree);
 
-    private AVLNode balanceTreeOnDelete(AVLNode aNode){
-        int balance = aNode.getBalance();
+        testAvlBinaryTree.delete(3);
+        System.out.println(testAvlBinaryTree);
 
-        // Case 1 : R0 and R1
-        if ( balance > 1 && aNode.getLeft().getBalance() >= 0){
-            return rightRotate(aNode);
-        }
-        // Case 2 : L-1 and L0
-        if (balance < -1 && aNode.getRight().getBalance() <= 0){
-            return leftRotate(aNode);
-        }
-        // Case 3 : R-1
-        if (balance > 1 && aNode.getLeft().getBalance() < 0) {
-            aNode.setLeft(leftRotate(aNode.getLeft()));
-            return rightRotate(aNode);
-        }
-        // Case 4 : L
-        if (balance < -1 && aNode.getRight().getBalance() > 0) {
-            aNode.setRight(rightRotate(aNode.getRight()));
-            return leftRotate(aNode);
-        }
-        return null;
-    }
+        testAvlBinaryTree.delete(4);
+        System.out.println(testAvlBinaryTree);
 
+        testAvlBinaryTree.insert(4);
+        System.out.println(testAvlBinaryTree);
 
-    private AVLNode rightRotate(AVLNode root) {
-        AVLNode child = root.getLeft();
-        AVLNode childRSubTree = child.getRight();
-        child.setRight(root);
-        root.setLeft(childRSubTree);
-        return child;
-    }
+        System.out.println(testAvlBinaryTree.search(10));
+        System.out.println(testAvlBinaryTree.search(11));
 
-    private AVLNode leftRotate(AVLNode root) {
-        AVLNode child = root.getRight();
-        AVLNode childLSubTree = child.getLeft();
-        child.setLeft(root);
-        root.setRight(childLSubTree);
-        return child;
-    }
-
-}
-
-
-abstract class BinaryTree<T extends Node<T>> {
-
-    private T root;
-    private final Supplier<? extends T> ctor;
-
-    public Stack<T> insert(int key){
-        Stack<T> stk = new Stack<>();
-        this.root = insert(key, this.root, stk);
-        return stk;
-    }
-
-    public Stack<T> delete(int key) {
-        Stack<T> stk = new Stack<>();
-        delete(key, this.root, stk);
-        return stk;
-    }
-
-    public Integer search(int key) {
-        T searchedNode = search(key, this.root);
-        return searchedNode != null ? searchedNode.getKey() : null;
-    }
-
-    public List<Integer> searchRange(int smallKey, int bigKey){
-        List<Integer> keys = new ArrayList<>();
-        searchRange(smallKey, bigKey, this.root, keys);
-        return keys;
-    }
-
-    @Override
-    public String toString() {
-        return traversePreOrder(this.root);
-    }
-
-
-    private T insert(int key, T root, Stack<T> stk){
-        if (root != null) {
-            stk.push(root);
-        }
-        if(root == null){
-            root = ctor.get();
-            root.setKey(key);
-            stk.push(root);
-            return root;
-        }
-        else if (key < root.getKey()){
-            root.setLeft(insert(key, root.getLeft(), stk));
-        }
-        else {
-            root.setRight(insert(key, root.getRight(), stk));
-        }
-        return root;
-    }
-
-    private T delete(int key, T root, Stack<T> stk) {
-
-        if (root == null) {
-            return null;
-        }
-        else {
-            if (key < root.getKey()){
-                stk.push(root);
-                root.setLeft(delete(key, root.getLeft(), stk));
-            }
-            else if (key > root.getKey()){
-                stk.push(root);
-                root.setRight(delete(key, root.getRight(), stk));
-            }
-            else {
-                if (root.getLeft() == null) {
-                    return root.getRight();
-                }
-                else if (root.getRight() == null) {
-                    return root.getLeft();
-                }
-                else {
-                    root.setKey(getMax(root.getLeft()));
-                    root.setLeft(delete(root.getKey(), root.getLeft(), stk));
-                }
-            }
-        }
-        return root;
-    }
-
-    private T search(int key, T root){
-        if(root == null || key == root.getKey()){
-            return root;
-        }
-        else if (key < root.getKey()){
-            return search(key, root.getLeft());
-        }
-        else {
-            return search(key, root.getRight());
-        }
-    }
-
-    private void searchRange(int smallKey, int bigKey, T root, List<Integer> keys) {
-        if (root == null) {
-            return;
-        }
-        if (smallKey < root.getKey()) {
-            searchRange( smallKey, bigKey, root.getLeft(), keys);
-        }
-        if (smallKey <= root.getKey() && bigKey >= root.getKey()) {
-            keys.add(root.getKey());
-        }
-        searchRange(smallKey, bigKey, root.getRight(), keys);
-    }
-
-    private int getMax(T root)
-    {
-        int max = root.getKey();
-        while (root.getRight() != null)
-        {
-            max = root.getRight().getKey();
-            root = root.getRight();
-        }
-        return max;
-    }
-
-    public String traversePreOrder(T root) {
-
-        if (root == null) {
-            return "";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(root.getKey());
-
-        String pointerRight = "└──";
-        String pointerLeft = (root.getRight() != null) ? "├──" : "└──";
-
-        traverseNodes(sb, "", pointerLeft, root.getLeft(), root.getRight() != null);
-        traverseNodes(sb, "", pointerRight, root.getRight(), false);
-
-        return sb.toString();
-    }
-
-    private void traverseNodes(StringBuilder sb, String padding, String pointer, T node, boolean hasRightSibling) {
-        if (node != null) {
-            sb.append("\n");
-            sb.append(padding);
-            sb.append(pointer);
-            sb.append(node.getKey());
-
-            StringBuilder paddingBuilder = new StringBuilder(padding);
-            if (hasRightSibling) {
-                paddingBuilder.append("│  ");
-            } else {
-                paddingBuilder.append("   ");
-            }
-
-            String paddingForBoth = paddingBuilder.toString();
-            String pointerRight = "└──";
-            String pointerLeft = (node.getRight() != null) ? "├──" : "└──";
-
-            traverseNodes(sb, paddingForBoth, pointerLeft, node.getLeft(), node.getRight() != null);
-            traverseNodes(sb, paddingForBoth, pointerRight, node.getRight(), false);
-        }
-    }
-
-    protected BinaryTree(Supplier<? extends T> ctor) {
-        this.ctor = Objects.requireNonNull(ctor);
-        this.root = null;
-    }
-
-    protected T getRoot() {
-        return root;
-    }
-
-    protected void setRoot(T root) {
-        this.root = root;
+        System.out.println(testAvlBinaryTree.searchRange(2, 6));
+        System.out.println(testAvlBinaryTree.searchRange(10, 20));
     }
 
 }
